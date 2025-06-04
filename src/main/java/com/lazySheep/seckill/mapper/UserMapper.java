c89d6c0fd0752afeaa94589305aa55fff0bc9eef
Lazysheep1108 <365353544@qq.com>
2025-06-04 22:35:47 +0800

    UserLogin related Class and mapperXml

---
+++ src/main/java/com/lazySheep/seckill/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.lazySheep.seckill.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.lazySheep.seckill.pojo.User;
+
+/**
+ * @author LazySheep
+ * @version V1.0
+ * @Package com.lazySheep.seckill.mapper
+ * @date 2025/6/4 22:30
+ * @project seckill
+ * @description
+ */
+public interface UserMapper extends BaseMapper<User> {
+}
