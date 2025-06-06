343122cbd97c91af7614b2af08468aa63451cd54
Lazysheep1108 <365353544@qq.com>
2025-06-06 10:56:04 +0800

    Login parameters

---
+++ src/main/java/com/lazySheep/seckill/vo/LoginVo.java
@@ -0,0 +1,19 @@
+package com.lazySheep.seckill.vo;
+
+import lombok.Data;
+
+/**
+ * @author LazySheep
+ * @version V1.0
+ * @Package com.lazySheep.seckill.vo
+ * @date 2025/6/6 10:54
+ * @project seckill
+ * @description Login parameters
+ */
+
+@Data
+public class LoginVo {
+
+    private String mobile;
+    private String password;
+}
